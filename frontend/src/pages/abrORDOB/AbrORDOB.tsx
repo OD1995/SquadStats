77e5c01819354b7e1d00ec3a64775c5287251758
Oli Dernie <oliverdernie1@googlemail.com>
2024-10-18 17:24:58 +0100

    Popups working

--- frontend/src/pages/abrORDOB/AbrORDOB.tsx
+++ frontend/src/pages/abrORDOB/AbrORDOB.tsx
@@ -1,57 +1,97 @@
-import Map from 'react-map-gl';
+import Map, { Marker } from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { MarkerData } from '../../types/MarkerData';
 import { AbrOrdobMarker } from './AbrOrdobMarker';
+import { AbrOrdobPopup } from './AbrOrdobPopup';
+import { useState } from 'react';
+import { data } from '@remix-run/router';
 
 const MAPBOX_TOKEN = "pk.eyJ1Ijoic3F1YWRzdGF0cyIsImEiOiJjbTJkbW5yeXcxN2d6MmlxeDlzY240aW0wIn0._5JU3L0HLagDhLjfyClqFQ"
 
-const dataArray : MarkerData[] = [
-    {
+const dataDict : Record<string,MarkerData> = {
+    "tim-japan": {
         latitude: 34.967519,
         longitude: 135.77971,
-        colour: 'red',
-        popupText: 'Test',
+        colour: 'blue',
+        popupText: `I spent an afternoon a couple of weeks back having to CORRECT every tour guide in my sight on what this great shrine actually stands for. Some of them had some absolutely laughable takes. Some stuff about a god of rice? 
+
+        Anyway... As any of us would, I launched into countless monologues about it's real historic standing. 
+
+The Fushimi Inari Shrine has a red arch for each time a significant moment has happened for ORDOB's 5 a side team in history.
+
+From John Walters first goal over a year ago, Mitch Gordon's last minute winner vs Palamara Jagon, Rob scoring 7 goals in one match, Joe having a beer and a Pad Thai before our first ever game and Mitch missing tonight because of JetLag.
+
+THIS IS WHY THESE BEAUTIFUL RED ARCHES EXIST
+
+Lets add another arch to make it 10,001 tonight ⛩️
+
+COYORDOB 🦏3️⃣👃`,
         markerId: "tim-japan"
     },
-    {
+    'robin-brazil': {
         latitude: -22.94962392506627,
         longitude: -43.20447905617719,
         colour: 'red',
-        popupText: 'Test2',
+        popupText: `Boys, it's the last of my telegrams on my scouting mission across the Americas. Unfortunately, I've found no-one with enough touch, charm and manners to even get a trial for our club. So the message for tonight from Christ the Redeemer in Rio is to play with freedom. Let the Seleção of our Thursday Castlehaven lineup take The Camden Kings by storm. Releasing our inner samba is as simple as rocking step combinations, rolling hip action, and pulsing movements whilst playing tika taka in syncopation. Look for the beautiful ball and play with your instincts, the favelas are with you 🎨🔫 #UDCDB🇧🇷 Um dia comum do barclays!`,
         markerId: 'robin-brazil'
     }
-]
+}
 
 export const AbrORDOB = () => {
 
-    // https://mariestarck.com/how-to-display-popups-on-a-mapbox-map-mapbox-react-tutorial-part-3/
+    const [markerDataDict, setMarkerDataDict] = useState<Record<string,MarkerData>>(dataDict);
+    const [showPopup, setShowPopup] = useState(false);
+    const [popupMarkerId, setPopupMarkerId] = useState("");
+    const [popupText, setPopupText] = useState("");
+
+    const handleClick = (markerId:string) => {
+        console.log("turning on")
+        setShowPopup(true);
+        setPopupMarkerId(markerId);
+        setPopupText(markerDataDict[markerId].popupText)
+    }
 
     return (
-        <Map
-            initialViewState={{
-                latitude: 51.510730,
-                longitude: -0.035190,
-                zoom: 1
-            }}
-            style={{
-                width: "90vw",
-                height: 600
-            }}
-            mapStyle="mapbox://styles/mapbox/streets-v9"
-            mapboxAccessToken={MAPBOX_TOKEN}
-        >
+        <div id='abrordob-parent'>
+            <Map
+                initialViewState={{
+                    latitude: 51.510730,
+                    longitude: -0.035190,
+                    zoom: 1
+                }}
+                style={{
+                    width: "65vw",
+                    height: 600
+                }}
+                mapStyle="mapbox://styles/mapbox/streets-v9"
+                mapboxAccessToken={MAPBOX_TOKEN}
+            >
+                {
+                    Object.values(dataDict).map(
+                        (data:MarkerData) => {
+                            return (
+                                <Marker
+                                    key={data.markerId}
+                                    latitude={data.latitude}
+                                    longitude={data.longitude}
+                                    color={data.colour}
+                                    // onClick={() => setShowPopup(!showPopup)}
+                                    onClick={() => handleClick(data.markerId)}
+                                />
+                            )
+                        }
+                    )
+                }
+            </Map>
             {
-                dataArray.map(
-                    (data:MarkerData) => {
-                        return (
-                            <AbrOrdobMarker
-                                key={data.markerId}
-                                data={data}
-                            />
-                        )
-                    }
+                showPopup && (
+                    <AbrOrdobPopup
+                        markerId={popupMarkerId}
+                        text={popupText}
+                        setShowPopup={setShowPopup}
+                    />
                 )
             }
-        </Map>
+        </div>
     );
 }
--- frontend/src/pages/abrORDOB/AbrOrdobPopup.tsx
+++ frontend/src/pages/abrORDOB/AbrOrdobPopup.tsx
@@ -1,29 +1,62 @@
-import { Popup as ReactMapGlPopup} from 'react-map-gl';
-import { MarkerData } from '../../types/MarkerData';
 import './AbrOrdobPopup.css';
 
 interface AbrOrdobPopupProps {
-    data: MarkerData
-    amShowing: boolean
+    markerId: string
+    text: string
+    setShowPopup: Function
 }
 
 export const AbrOrdobPopup = (props:AbrOrdobPopupProps) => {
-    return (
-        <ReactMapGlPopup
-            className='abrordob-popup'
-            anchor='top'
-            latitude={props.data.latitude}
-            longitude={props.data.longitude}
-        >
-            <div id={props.data.markerId}>
-                <img
-                    width="100%"
-                    src={"/abrORDOB/" + props.data.markerId + ".jpeg"}
-                />
-                <p>
-                    {props.data.popupText}
+
+    const buildText = (text:string) => {
+        const paragraphs = text.split(/\r?\n/);
+        var returnMe = [];
+        var key = 0;
+        for (const para of paragraphs) {
+            returnMe.push(
+                <p key={key}>
+                    {para}
                 </p>
+            );
+            key += 1;
+        }
+        return returnMe;
+    }
+
+    return (
+        <div
+            id='abrordob-popup-parent'
+            onClick={() => props.setShowPopup(false)}
+        >
+            <div
+                id='abrordob-popup-content'
+                onClick={(e) => e.stopPropagation()}
+            >
+                <div id='abrordob-top'>
+                    <span
+                        id='abrordob-popup-close'
+                        onClick={() => props.setShowPopup(false)}
+                    >
+                        &times;
+                    </span>
+                </div>
+                <div id='abrordob-bottom'>
+                    <div
+                        className='abrorodb-popup-obj'
+                    >
+                        <img
+                            id='abrordob-popup-img'
+                            src={"/abrORDOB/" + props.markerId + ".jpeg"}
+                        />
+                    </div>
+                    <div
+                        id='abrordob-popup-text'
+                        className='abrorodb-popup-obj'
+                    >
+                        {buildText(props.text)}
+                    </div>
+                </div>
             </div>
-        </ReactMapGlPopup>
+        </div>
     )
 }
